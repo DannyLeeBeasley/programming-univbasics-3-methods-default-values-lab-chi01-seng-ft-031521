abc7645461fa2b254ee316b759b0d3061ededd30
Daniel Lee Beasley <dannyleebeasley@gmail.com>
2021-03-02 20:04:23 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,6 +3,6 @@ puts "What a nutritious meal!"
 puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
-meal_choice()
+meal_choice('carrots', 'string beans', 'chicken')
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
